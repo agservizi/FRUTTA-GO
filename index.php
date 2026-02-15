a4e791c73dd1bfb1d46d545a9b46495e2c8757e1
agservizi <ag.servizi16@gmail.com>
2026-02-15 15:20:25 +0100

    Aggiungi configurazione per la visualizzazione degli errori in index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 // index.php nella root - Reindirizza a public/ o mostra un messaggio
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 header('Location: public/');
 exit;
 ?>
